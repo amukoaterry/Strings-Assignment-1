8bebf298891d82678f3f633abfbaafe5911005aa
amukoaterry <amukoaterrymitchell@gmail>
2024-03-08 20:10:56 +0300

    add files

---
+++ strings.js
@@ -0,0 +1,54 @@
+//1.)Extract the last four characters from the string below;"extravaganza"
+
+let a = `Extravaganza`
+
+console.log(`item`,a[11]);
+console.log(`item`,a[10]);
+console.log(`item`,a[9]);
+console.log(`item`,a[8]);
+
+//2.)Insert the following string at the fourth index of the below variable."eat" const food ="The quick fox jumped over the lazy dog"
+
+const food ="The quiclk fox jumped over the lazy dog";
+console.log(food.slice(0,4) + "eat" + food.slice(4));
+
+
+//3.)Count how many times the following string appears in the string  variable:1"the"2"brown" const story="The quick brown fox "
+
+const story = "The quick brown fox jumped over the lazy dog";
+
+console.log(story.match(/the/g).length);
+console.log(story.match(/brown/g).length);
+
+//4.)                                //
+const string1="The pupils are reading in the library";
+console.log(string1[11] +""+string1[12]+""+string1[13]);
+
+
+//5.)
+
+
+
+//6.)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
